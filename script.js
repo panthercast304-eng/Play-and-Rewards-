349cdae5757ff1f69242c9493a3634f87176f868
panthercast304-eng <panthercast304@gmail.com>
2026-09-08 17:26:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -61,3 +61,8 @@ function render(){
 }
 render();
 showPage('home');
+// Supabase connection
+const supabaseClient = window.supabase.createClient(
+  window.APP_CONFIG.SUPABASE_URL,
+  window.APP_CONFIG.SUPABASE_ANON_KEY
+);
